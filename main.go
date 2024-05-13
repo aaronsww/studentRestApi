e027b5fba39748e1ad31e1b2b7331451cac820fa
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2024-05-13 08:11:23 +0530

    chore: integrated database and createStudent api

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
+
 	"net/http"
 
 	"errors"
 
 	"github.com/gin-gonic/gin"
+
+	"database/sql"
+
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "testdbee"
 )
 
 type student struct {
@@ -28,7 +45,7 @@ func studentById(c *gin.Context) {
 	student, err := getStudentById(id)
 
 	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.h{"message": "Book not found."})
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Student not found."})
 		return
 	}
 
@@ -45,21 +62,53 @@ func getStudentById(id string) (*student, error) {
 	return nil, errors.New("student not found")
 }
 
-func createStudent(c *gin.Context) {
+func createStudent(c *gin.Context, db *sql.DB) {
 	var newStudent student
 
 	if err := c.BindJSON(&newStudent); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	students = append(students, newStudent)
-	c.IndentedJSON(http.StatusCreated, newStudent)
+	// Insert data into the database
+	result, err := db.Exec("INSERT INTO students (name) VALUES ($1)", newStudent.Name)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, result)
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, dbPassword, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
 	router := gin.Default()
 	router.GET("/students", getStudents)
 	router.GET("/students/:id", studentById)
-	router.POST("/students", createStudent)
+	router.POST("/students", func(c *gin.Context) {
+		createStudent(c, db)
+	})
 	router.Run("localhost:8080")
 }
